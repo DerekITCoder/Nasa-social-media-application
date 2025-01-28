0efae09b70b7801b86af56b83dfe01c15f7d6f94
Derek Dickerson <derekdson67@gmail.com>
2025-01-28 11:51:06 -0600

    first

---
+++ nasa-social-media-app/backend/src/main/java/com/derek/nasa_social_media_app/NasaSocialMediaAppApplication.java
@@ -0,0 +1,13 @@
+package com.derek.nasa_social_media_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NasaSocialMediaAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NasaSocialMediaAppApplication.class, args);
+	}
+
+}
---
+++ nasa-social-media-app/backend/src/test/java/com/derek/nasa_social_media_app/NasaSocialMediaAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.derek.nasa_social_media_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NasaSocialMediaAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
