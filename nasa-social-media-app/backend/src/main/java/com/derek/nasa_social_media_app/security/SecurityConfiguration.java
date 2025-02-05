251e8987e876072cc9c7b2d5a848c148406291d2
Derek Dickerson <derekdson67@gmail.com>
2025-02-05 17:47:35 -0600

    sign in screen work

--- nasa-social-media-app/backend/src/main/java/com/derek/nasa_social_media_app/security/SecurityConfiguration.java
+++ nasa-social-media-app/backend/src/main/java/com/derek/nasa_social_media_app/security/SecurityConfiguration.java
@@ -1,24 +1,30 @@
 package com.derek.nasa_social_media_app.security;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractAuthenticationFilterConfigurer;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+
 public class SecurityConfiguration {
 
-//  @Bean
-//     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
-//         return httpSecurity
-//                 .csrf(AbstractHttpConfigurer::disable)
-//                 .authorizeHttpRequests(registry -> {
+ @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+        return httpSecurity
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(registry -> {
 
-//                 registry.anyRequest().authenticated();
+                registry.anyRequest().authenticated();
 
-//             })
-//             .formLogin(httpSecurityFormLoginConfigurer -> {
-//                 httpSecurityFormLoginConfigurer
-//                         .loginPage("/login")
-//                         .successHandler(new AuthenticationSuccessHandler())
-//                         .permitAll();
-//             })
-//             .build();
-// }
+            })
+            .formLogin(AbstractAuthenticationFilterConfigurer::permitAll)
+            .build();
+                
+                    
+            
+          
+}
 
 
 
