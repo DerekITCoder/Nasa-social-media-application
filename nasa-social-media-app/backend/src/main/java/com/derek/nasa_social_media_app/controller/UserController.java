1cfc977465e82ef2732d0d89c5f98c2d728397f3
Derek Dickerson <derekdson67@gmail.com>
2025-02-11 01:39:17 -0600

    login page works

--- nasa-social-media-app/backend/src/main/java/com/derek/nasa_social_media_app/controller/UserController.java
+++ nasa-social-media-app/backend/src/main/java/com/derek/nasa_social_media_app/controller/UserController.java
@@ -2,6 +2,8 @@ package com.derek.nasa_social_media_app.controller;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -11,38 +13,38 @@ import com.derek.nasa_social_media_app.component.UserService;
 import com.derek.nasa_social_media_app.model.UserProfile;
 import com.derek.nasa_social_media_app.repository.UserProfileRepository;
 
-@RestController
+@Controller
 public class UserController {
     
+  // @Autowired
+  // private UserProfileRepository repository;
 
-    private UserProfileRepository repository;
-
-    private UserService service;
-
-
-    public UserController(UserProfileRepository repository,UserService service) {
-        this.repository = repository;
-        this.service = service;
-    }
+  
 
 
-    @GetMapping("/users/get")
-    public List<UserProfile> getAllUsers() {
-        return (List<UserProfile>) repository.findAll();
-    }
 
 
-    @PostMapping("/users/save")
-    public UserProfile createUser(@RequestBody UserProfile userProfile) {
-        return repository.save(userProfile);
-    }
+
+    // @GetMapping("/users/get")
+    // public List<UserProfile> getAllUsers() {
+    //     return (List<UserProfile>) repository.findAll();
+    // }
+
+
+    // @PostMapping("/users/save")
+    // public UserProfile createUser(@RequestBody UserProfile userProfile) {
+    //     return repository.save(userProfile);
+    // }
 
     @GetMapping("/users/home")
     public String handleUserHome() {
       return "home_users";
     }
 
-
+    @GetMapping("/login")
+    public String loginPage() {
+      return "loginPage";
+    }
 
 
 }
