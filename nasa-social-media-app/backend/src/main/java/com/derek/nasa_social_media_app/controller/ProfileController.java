89b4ed991ce31c79c7790074b22fc61f64a8883e
Derek Dickerson <derekdson67@gmail.com>
2025-02-12 20:38:17 -0600

    login fix

--- nasa-social-media-app/backend/src/main/java/com/derek/nasa_social_media_app/controller/ProfileController.java
+++ nasa-social-media-app/backend/src/main/java/com/derek/nasa_social_media_app/controller/ProfileController.java
@@ -1,6 +1,68 @@
 package com.derek.nasa_social_media_app.controller;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.derek.nasa_social_media_app.component.UserService;
+import com.derek.nasa_social_media_app.model.UserProfile;
+import com.derek.nasa_social_media_app.repository.UserProfileRepository;
+
+
+@RestController
 public class ProfileController {
     
+
+    @Autowired
+    private UserProfileRepository repository;
+
+  
+
+
+
+
+
+
+    @GetMapping("/users/get")
+    public List<UserProfile> getAllUsers() {
+        return (List<UserProfile>) repository.findAll();
+    }
+
+
+    @PostMapping("/users/save")
+    public UserProfile createUser(@RequestBody UserProfile userProfile) {
+        return repository.save(userProfile);
+    }
+
+    @GetMapping("/home")
+    public String handleUserHome() {
+      return "home_users";
+    }
+
+
+    @GetMapping("/names")
+    public List<String> findAllOnlyNames() {
+    List<UserProfile> users = (List<UserProfile>) repository.findAll();
+    return users.stream()
+            .map(UserProfile::getUsername)
+            .collect(Collectors.toList());
+}
+    
+    
+
+
+
+
+
+
+
+
+
+
 
 }
